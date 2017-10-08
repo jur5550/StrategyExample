491cf2c1fa40d45d80a153e7538f381cd488c59b
Jur van Zonneveld <jurvanzonneveld@gmail.com>
2017-10-08 16:33:18 +0200

    Update

--- src/ShoppingCartTest.java
+++ src/ShoppingCartTest.java
@@ -6,8 +6,9 @@ public class ShoppingCartTest {
         ShoppingCart cart = new ShoppingCart();
 
         Scanner reader = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        System.out.println("Price for first item: ");
         int n = reader.nextInt();
+        System.out.println("Price for second item: ");
         int m = reader.nextInt();
         reader.close();
         Item item1 = new Item("Banana",n);
@@ -20,9 +21,9 @@ public class ShoppingCartTest {
 
 
         //pay by paypal
-        cart.pay(new PaypalStrategy("myemail@example.com", "mypwd"));
+        cart.pay(new PaypalStrategy("test@example.com", "password"));
         //pay by credit card
-        cart.pay(new CreditCardStrategy("Jur van Zonneveld", "1234567890123456", "786", "12/15"));
+        cart.pay(new CreditCardStrategy("Jur van Zonneveld", "CardNumber", "786", "Mei 2018"));
     }
 
 }
